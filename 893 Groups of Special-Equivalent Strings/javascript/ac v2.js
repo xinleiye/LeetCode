c4a9678a1e8c40d3eac3d3b84c23fc723f50aab4
yexinlei <xinlei_ye@hotmail.com>
2019-07-02 11:35:16 +0800

    893 特殊等价字符串组ac

--- 893 Groups of Special-Equivalent Strings/javascript/ac v2.js
+++ 893 Groups of Special-Equivalent Strings/javascript/ac v2.js
@@ -8,11 +8,11 @@
 var numSpecialEquivGroups = function(A) {
     let strVal = "";
     let res = new Map();
-    
+
     function getStringVal (str) {
         let even = [];
         let odd = [];
-        
+
         for (let i = 0, len = str.length; i < len; i++) {
             if (i % 2) {
                 odd.push(str[i]);
@@ -22,7 +22,7 @@ var numSpecialEquivGroups = function(A) {
         }
         return odd.sort().join("") + even.sort().join("");
     }
-    
+
     for (let i = 0, len = A.length; i < len; i++) {
         strVal = getStringVal(A[i]);
         if (res.has(strVal)) {
@@ -31,6 +31,6 @@ var numSpecialEquivGroups = function(A) {
             res.set(strVal, 1);
         }
     }
-    
+
     return res.size;
 };
