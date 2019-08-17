4f031e488344777585e410eadfd9b8f121b6fa42
yexinlei <xinlei_ye@hotmail.com>
2019-08-17 20:53:42 +0800

    617 合并二叉树 循环ac
    循环版需要点小技巧。

---
+++ 617 Merge Two Binary Trees/javascript/ac_v3.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ * @Runtime 88 ms, faster than 76.01% of JavaScript online submissions for Merge Two Binary Trees.
+ * @Memory_Usage 41.2 MB, less than 7.69% of JavaScript online submissions for Merge Two Binary Trees.
+ */
+var mergeTrees = function(t1, t2) {
+    let nodes1 = [];
+    let nodes2 = [];
+
+    if (t1 === null) {
+        return t2;
+    }
+
+    nodes1.push(t1);
+    nodes2.push(t2);
+
+    while (nodes1.length || nodes2.length) {
+        let n1 = nodes1.pop();
+        let n2 = nodes2.pop();
+
+        if (n1 === null || n2 === null) {
+            continue;
+        }
+        n1.val += n2.val;
+        if (n1.left === null) {
+            n1.left = n2.left;
+        } else {
+            nodes1.push(n1.left);
+            nodes2.push(n2.left);
+        }
+        if (n1.right === null) {
+            n1.right = n2.right;
+        } else {
+            nodes1.push(n1.right);
+            nodes2.push(n2.right);
+        }
+    }
+
+    return t1;
+};
