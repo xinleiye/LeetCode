a3f7e384c90518f237ccec8a9fb9dea132e99f8e
yexinlei <xinlei_ye@hotmail.com>
2019-10-20 23:49:38 +0800

    712 两个字符串的最小ASCII删除和 C语言ac

--- 712 Minimum ASCII Delete Sum for Two Strings/c/ac_v1.c
+++ 712 Minimum ASCII Delete Sum for Two Strings/c/ac_v1.c
@@ -2,10 +2,12 @@ int minimumDeleteSum(char * s1, char * s2){
     int res = 0;
     int len1 = strlen(s1);
     int len2 = strlen(s2);
+    // 这个地方有点意思，旧版的C是不支持这种数组定义方法的
     int sum[len2 + 1][len1 + 1];
     int col, row;
 
-    memset(sum, 0, sizeof(int) * (len1 + 1) * (len2 + 1));
+    // 此处内存长度计算错误，少乘以sizeof(int)
+    memset(sum, 0, (len1 + 1) * (len2 + 1));
     for (col = 0; col < len1; col++) {
         sum[0][col + 1] = sum[0][col] + s1[col];
     }
