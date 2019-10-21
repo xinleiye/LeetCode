3739360a6e45cc4575f2124868cbaec2e5830f6e
xinleiye <xinlei_ye@163.com>
2019-10-21 11:48:34 +0800

    687 最长同值路径 ac

---
+++ 687 Longest Univalue Path/javascript/ac_v2.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestUnivaluePath = function(root) {
+    let res = 0;
+    let nodes = [];
+    let getHeightVal = function (node, preVal) {
+        let lLen = 0;
+        let rLen = 0;
+
+        if (!node) {
+            return 0;
+        }
+
+        lLen = getHeightVal(node.left, node.val);
+        rLen = getHeightVal(node.right, node.val);
+        res = Math.max(res, lLen + rLen);
+
+        if (node.val === preVal) {
+            return lLen > rLen ? lLen + 1 : rLen + 1;
+        } else {
+            return 0;
+        }
+    }
+
+    if (!root) {
+        return res;
+    }
+    getHeightVal(root, root.val);
+
+    return res;
+};
