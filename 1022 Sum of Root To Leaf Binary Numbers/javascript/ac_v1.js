74ffaf8e6245e69fb7ef277ed2365d66ca40c17f
xinleiye <xinlei_ye@163.com>
2019-10-23 13:53:06 +0800

    1022 从根到叶的二进制数之和 ac

---
+++ 1022 Sum of Root To Leaf Binary Numbers/javascript/ac_v1.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+    let res = 0;
+    let getSum = function (node, sum) {
+        if (!node.left && !node.right) {
+            res += (sum << 1) + node.val;
+            return;
+        }
+        if (node.left) {
+            // + 优先级高于 <<
+            getSum(node.left, (sum << 1) + node.val);
+        }
+        if (node.right) {
+            getSum(node.right, (sum << 1) + node.val);
+        }
+    }
+
+    getSum(root, 0);
+
+    return res;
+};
