7506506b703b019f9aaf29121e1e928fff8aa539
yexinlei <xinlei_ye@hotmail.com>
2019-12-15 21:13:17 +0800

    第 167 场周赛

---
+++ 第 167 场周赛/5284. 顺次数/javascript/ac_v1.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+    let res = [];
+    let digitsTable = [
+        [12, 23, 34, 45, 56, 67, 78, 89],
+        [123, 234, 345, 456, 567, 678, 789],
+        [1234, 2345, 3456, 4567, 5678, 6789],
+        [12345, 23456, 34567, 45678, 56789],
+        [123456, 234567, 345678, 456789],
+        [1234567, 2345678, 3456789],
+        [12345678, 23456789],
+        [123456789]
+    ];
+
+    for (let i = 0; i < digitsTable.length; i++) {
+        let digits = digitsTable[i];
+
+        for (let j = 0; j < digits.length; j++) {
+            if (digits[j] >= low && digits[j] <= high) {
+                res.push(digits[j]);
+            }
+        }
+    }
+
+    return res;
+};
