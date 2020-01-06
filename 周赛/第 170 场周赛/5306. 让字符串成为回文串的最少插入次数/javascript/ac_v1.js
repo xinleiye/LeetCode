7fe06c8a4288292940fbd8c2a552909bee5fffed
yexinlei <xinlei_ye@hotmail.com>
2020-01-06 15:56:21 +0800

    "第 170 场周赛 5306 ac"

--- 周赛/第 170 场周赛/5306. 让字符串成为回文串的最少插入次数/javascript/ac_v1.js
+++ 周赛/第 170 场周赛/5306. 让字符串成为回文串的最少插入次数/javascript/ac_v1.js
@@ -1,8 +1,23 @@
 /**
- * @param {string[]} words
- * @param {string} result
- * @return {boolean}
+ * @param {string} s
+ * @return {number}
  */
-var isSolvable = function(words, result) {
-    
+var minInsertions = function(s) {
+    let insertCount = new Array(s.length);
+
+    for (let i = 0; i < s.length; i++) {
+        insertCount[i] = (new Array(s.length)).fill(0);
+    }
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let j = i + 1; j < s.length; j++) {
+            if (s[i] === s[j]) {
+                insertCount[i][j] = insertCount[i + 1][j - 1];
+            } else {
+                insertCount[i][j] = Math.min(insertCount[i + 1][j], insertCount[i][j - 1]) + 1;
+            }
+        }
+    }
+
+    return insertCount[0][s.length - 1];
 };
