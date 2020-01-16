2d567a75da57331103ff4d9944aad5f8887eb90a
xinleiye <xinlei_ye@163.com>
2020-01-16 11:42:05 +0800

    第 171 场周赛 二指输入的最小距离 ac

--- 周赛/第 171 场周赛/5310. 二指输入的的最小距离/javascript/ac_v1.js
+++ 周赛/第 171 场周赛/5310. 二指输入的的最小距离/javascript/ac_v1.js
@@ -4,38 +4,35 @@
  */
 var minimumDistance = function(word) {
     let res = Infinity;
-    let distanceMap = new Array(word.length);
+    let distanceMap = new Array(word.length + 1);
     let getDistance = (src, tar) => {
-        return Math.abs((src % 6 - tar % 6) + (src - src % 6) / 6 - (tar - tar % 6) / 6);
+        return Math.abs(src % 6 - tar % 6) + Math.abs((src - src % 6) / 6 - (tar - tar % 6) / 6);
     };
 
     for (let i = 0; i <= word.length; i++) {
-        distanceMap[i] = new Array(26)
+        distanceMap[i] = new Array(26);
     }
-    for (let i = 0; i <= word.length; i++) {
-        for (let k = 0; k < 26; k++) {
-            distanceMap[i][k] = (new Array(26)).fill(100);
-        }
-    }
-
     for (let i = 0; i < 26; i++) {
-        for (let j = 0; j < 26; j++) {
-            distanceMap[0][i][j] = getDistance(i, word.charCodeAt(0) - 65);
-            // distanceMap[0][i][j] = getDistance(j, word.charCodeAt(0) - 65);
+        distanceMap[0][i] = (new Array(26)).fill(0);
+    }
+    for (let i = 1; i <= word.length; i++) {
+        for (let k = 0; k < 26; k++) {
+            distanceMap[i][k] = (new Array(26)).fill(Infinity);
         }
     }
 
-    for (let i = 1, len = word.length; i < len; i++) {
-        let tar = word.charCodeAt(i) - 65;
+    for (let i = 1, len = word.length; i <= len; i++) {
+        let tar = word.charCodeAt(i - 1) - 65;
 
         for (let j = 0; j < 26; j++) {
             for (let k = 0; k < 26; k++) {
-                if (distanceMap[i - 1][j][k] < 100) {
-
+                if (distanceMap[i - 1][j][k] !== Infinity) {
+                    // 左指头
+                    distanceMap[i][tar][k] = Math.min(distanceMap[i][tar][k], distanceMap[i - 1][j][k] + getDistance(j, tar));
+                    // 右指头
+                    distanceMap[i][j][tar] = Math.min(distanceMap[i][j][tar], distanceMap[i - 1][j][k] + getDistance(k, tar));
                 }
-                distanceMap[i][j][k] = getDistance(i, word.charCodeAt(0) - 65);
-                // distanceMap[0][i][j] = getDistance(j, word.charCodeAt(0) - 65);
-                if (i === word.length - 1) {
+                if (i === word.length) {
                     res = Math.min(res, distanceMap[i][tar][k]);
                     res = Math.min(res, distanceMap[i][j][tar]);
                 }
@@ -43,10 +40,5 @@ var minimumDistance = function(word) {
         }
     }
 
-    for (let i = 0; i < 26; i++) {
-        res = Math.min(res, distanceMap[i][tar][k]);
-        res = Math.min(res, distanceMap[i][j][tar]);
-    }
-    console.log(distanceMap[1]);
     return res;
 };
