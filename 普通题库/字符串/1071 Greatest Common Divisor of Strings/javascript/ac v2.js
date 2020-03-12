77a5763dc9e96a9dbcd688c8be0b1b90238136b7
xinleiye <xinlei_ye@163.com>
2020-03-12 17:11:59 +0800

    每日一题

--- 普通题库/字符串/1071 Greatest Common Divisor of Strings/javascript/ac v2.js
+++ 普通题库/字符串/1071 Greatest Common Divisor of Strings/javascript/ac v2.js
@@ -2,8 +2,6 @@
  * @param {string} str1
  * @param {string} str2
  * @return {string}
- * @Runtime 56 ms, faster than 71.78% of JavaScript online submissions for Greatest Common Divisor of Strings.
- * @Memory_Usage 33.8 MB, less than 100.00% of JavaScript online submissions for Greatest Common Divisor of Strings.
  */
 var gcdOfStrings = function(str1, str2) {
     if (str1.length < str2.length) {
