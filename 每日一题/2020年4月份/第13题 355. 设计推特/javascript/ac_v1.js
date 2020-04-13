b35f89d3e51966f218d6740b2b3e96c38fe28a44
yexinlei <xinlei_ye@hotmail.com>
2020-04-13 23:59:24 +0800

    每日一题 设计推特

---
+++ 每日一题/2020年4月份/第13题 355. 设计推特/javascript/ac_v1.js
@@ -0,0 +1,137 @@
+function Tweet (userId, tweetId) {
+    this.tweetId = tweetId;
+    this.userId = userId;
+    this.date = new Date().getTime();
+}
+
+function UserInfo (userId) {
+    this.userId = userId;
+    this.followerIds = new Set();
+    this.followeeIds = new Set();
+    this.myTweets = [];
+    this.lastTweets = [];
+}
+
+function updateUserLastTweets (map, userId, tweet) {
+    let userInfo = map.get(userId);
+
+    if (userInfo.lastTweets.length >= 10) {
+        userInfo.lastTweets.pop();
+    }
+    userInfo.lastTweets.unshift(tweet);
+}
+/**
+ * Initialize your data structure here.
+ */
+var Twitter = function() {
+    this.userMap = new Map();
+};
+
+/**
+ * Compose a new tweet. 
+ * @param {number} userId 
+ * @param {number} tweetId
+ * @return {void}
+ */
+Twitter.prototype.postTweet = function(userId, tweetId) {
+    let tweet = new Tweet(userId, tweetId);
+
+    if (this.userMap.has(userId)) {
+        let userInfo = this.userMap.get(userId);
+
+        userInfo.myTweets.push(tweet);
+        userInfo.followerIds.forEach((key) => {
+            updateUserLastTweets(this.userMap, key, tweet);
+        });
+    } else {
+        let newUser = new UserInfo(userId);
+
+        newUser.myTweets.push(tweet);
+        this.userMap.set(userId, newUser);
+    }
+    updateUserLastTweets(this.userMap, userId, tweet);
+};
+
+/**
+ * Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. 
+ * @param {number} userId
+ * @return {number[]}
+ */
+Twitter.prototype.getNewsFeed = function(userId) {
+    if (this.userMap.has(userId)) {
+        return this.userMap.get(userId).lastTweets.map(tweet => {
+            return tweet.tweetId;
+        });
+    } else {
+        return [];
+    }
+};
+
+/**
+ * Follower follows a followee. If the operation is invalid, it should be a no-op. 
+ * @param {number} followerId 
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.follow = function(followerId, followeeId) {
+    let tweets = [];
+    let userInfo;
+
+    if (followerId === followeeId) {
+        return;
+    }
+
+    if (!this.userMap.has(followerId)) {
+        userInfo = new UserInfo(followerId);
+        this.userMap.set(followerId, userInfo);
+    } else {
+        userInfo = this.userMap.get(followerId);
+    }
+
+    userInfo.followeeIds.add(followeeId);
+    if (!this.userMap.has(followeeId)) {
+        this.userMap.set(followeeId, new UserInfo(followeeId));
+    } else {
+        userInfo.lastTweets = userInfo.lastTweets.concat(this.userMap.get(followeeId).myTweets.slice(-10)).sort((tweet1, tweet2) => {
+            return tweet1.date - tweet2.date;
+        }).slice(-10);
+    }
+
+    this.userMap.get(followeeId).followerIds.add(followerId);
+};
+
+/**
+ * Follower unfollows a followee. If the operation is invalid, it should be a no-op. 
+ * @param {number} followerId 
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.unfollow = function(followerId, followeeId) {
+    let tweets = [];
+    let userInfo = this.userMap.get(followerId);
+
+    if (followerId === followeeId) {
+        return;
+    }
+
+    userInfo.followeeIds.delete(followeeId);
+
+    userInfo.followeeIds.forEach(val => {
+        tweets = tweets.concat(this.userMap.get(val).myTweets.slice(-10));
+    });
+    tweets = tweets.concat(userInfo.myTweets);
+    userInfo.lastTweets = tweets.sort((tweet1, tweet2) => {
+        return tweet1.date - tweet2.date;
+    }).slice(-10);
+
+    this.userMap.get(followeeId).followerIds.delete(followerId);
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * var obj = new Twitter()
+ * obj.postTweet(userId,tweetId)
+ * var param_2 = obj.getNewsFeed(userId)
+ * obj.follow(followerId,followeeId)
+ * obj.unfollow(followerId,followeeId)
+ */
