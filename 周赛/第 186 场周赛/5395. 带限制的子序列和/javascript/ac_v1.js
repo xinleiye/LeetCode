48aa5f80c5a431f1154d694311f3b9aba7564ea6
xinleiye <xinlei_ye@163.com>
2020-04-28 15:45:02 +0800

    第 186 场周赛 带限制的子序列和ac

--- 周赛/第 186 场周赛/5395. 带限制的子序列和/javascript/ac_v1.js
+++ 周赛/第 186 场周赛/5395. 带限制的子序列和/javascript/ac_v1.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
 var constrainedSubsetSum = function(nums, k) {
-    
+    let res = nums[0];
+    let sumK = [];
+    let sum = nums[0];
+
+    sumK.push(nums[0]);
+    for (let i = 1; i < nums.length; i++) {
+        sum = Math.max(nums[i], sumK[0] + nums[i]);
+        res = Math.max(res, sum);
+
+        if (sumK.length === k) {
+            sumK.shift();
+        }
+        while (sumK.length > 0 && sumK[sumK.length - 1] < sum) {
+            sumK.pop();
+        }
+        sumK.push(sum);
+    }
+
+    return res;
 };
