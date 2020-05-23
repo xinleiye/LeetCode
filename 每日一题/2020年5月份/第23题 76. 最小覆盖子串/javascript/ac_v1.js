5f83ebe0b7c9a0fb01e348beb42d119972392fc5
xinlei_ye <xinlei_ye@163.com>
2020-05-23 23:30:30 +0800

    每日一题 最小覆盖子串

--- 每日一题/2020年5月份/第23题 76. 最小覆盖子串/javascript/ac_v1.js
+++ 每日一题/2020年5月份/第23题 76. 最小覆盖子串/javascript/ac_v1.js
@@ -1,33 +1,8 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
  */
-/**
- * @param {number[]} preorder
- * @param {number[]} inorder
- * @return {TreeNode}
- */
-var buildTree = function(preorder, inorder) {
-    let rootIndex = 0;
-    let getTree = (inLeft, inRight) => {
-        let node;
-        let index;
-        
-        if (inLeft > inRight) {
-            return null;
-        }
-        
-        node = new TreeNode(preorder[rootIndex]);
-        index = inorder.indexOf(preorder[rootIndex]);
-        rootIndex++;
-        node.left = getTree(inLeft, index - 1);
-        node.right = getTree(index + 1, inRight);
-        
-        return node;
-    };
+var minWindow = function(s, t) {
 
-    return getTree(0, inorder.length - 1);
 };
