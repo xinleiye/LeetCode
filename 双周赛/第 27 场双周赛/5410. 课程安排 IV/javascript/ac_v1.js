4cfc9f72dba99034a8d23ff22d9cfd23165f7f76
xinlei_ye <xinlei_ye@163.com>
2020-05-31 20:32:20 +0800

    第 27 场双周赛

---
+++ 双周赛/第 27 场双周赛/5410. 课程安排 IV/javascript/ac_v1.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @param {number[][]} prerequisites
+ * @param {number[][]} queries
+ * @return {boolean[]}
+ */
+var checkIfPrerequisite = function(n, prerequisites, queries) {
+
+};
