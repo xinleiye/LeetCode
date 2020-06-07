bd04572cd582eaf5e05c8893b84b1366bcd04ccb
xinlei_ye <xinlei_ye@163.com>
2020-06-07 21:56:18 +0800

    每日一题 单词接龙 II

--- 每日一题/2020年6月份/第4题 238. 除自身以外数组的乘积/javascript/ac_v1.js
+++ 每日一题/2020年6月份/第4题 238. 除自身以外数组的乘积/javascript/ac_v1.js
@@ -3,5 +3,23 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+    let res = new Array(nums.length);
+    let multiFromEnd = new Array(nums.length + 1);
+    let multiFromStart = new Array(nums.length + 1);
 
+    multiFromEnd[multiFromEnd.length - 1] = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        multiFromEnd[i] = nums[i] * multiFromEnd[i + 1];
+    }
+
+    multiFromStart[0] = 1;
+    for (let i = 0; i < nums.length; i++) {
+        multiFromStart[i + 1] = nums[i] * multiFromStart[i];
+    }
+
+    for (let i = 1; i <= nums.length; i++) {
+        res[i - 1] = multiFromStart[i - 1] * multiFromEnd[i];
+    }
+
+    return res;
 };
