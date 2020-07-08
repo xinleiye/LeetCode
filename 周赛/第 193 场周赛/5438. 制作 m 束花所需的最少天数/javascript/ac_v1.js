17cd5b2ad9bd5ca12a9470a6b51c6bc33ff57bc7
yexinlei <xinlei_ye@hotmail.com>
2020-07-08 11:00:09 +0800

    第193 场周赛 制作 m 束花所需的最少天数

--- 周赛/第 193 场周赛/5438. 制作 m 束花所需的最少天数/javascript/ac_v1.js
+++ 周赛/第 193 场周赛/5438. 制作 m 束花所需的最少天数/javascript/ac_v1.js
@@ -5,5 +5,54 @@
  * @return {number}
  */
 var minDays = function(bloomDay, m, k) {
+    let res = -1;
+    let bloomed = new Array(bloomDay.length).fill(false);
+    let isM = (day, m) => {
+        let pre = -1;
 
+        for (let i = 0; i < bloomDay.length; i++) {
+            bloomed[i] = bloomDay[i] <= day;
+        }
+
+        for (let i = 0; i < bloomed.length; i++) {
+            // 未使用花朵的起始位置
+            if (bloomed[i]) {
+                if (pre === -1) {
+                    pre = i;
+                }
+            } else {
+                pre = -1;
+                continue;
+            }
+            if ((i - pre + 1) === k) {
+                m--;
+                pre = -1;
+                if (m === 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    };
+    let left = 1e9;
+    let right = 1;
+
+    bloomDay.forEach((val) => {
+        right = Math.max(right, val);
+        left = Math.min(left, val);
+    });
+
+    while (left <= right) {
+        let mid = (left + right) >>> 1;
+
+        if (isM(mid, m)) {
+            res = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return res;
 };
