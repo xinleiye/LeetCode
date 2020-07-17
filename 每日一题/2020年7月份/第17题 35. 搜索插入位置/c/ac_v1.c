aac052ec05d7721de52491b5639908ce44962f24
xinleiye <xinlei_ye@163.com>
2020-07-17 11:04:24 +0800

    每日一题 搜索插入位置

---
+++ 每日一题/2020年7月份/第17题 35. 搜索插入位置/c/ac_v1.c
@@ -0,0 +1,18 @@
+int searchInsert(int* nums, int numsSize, int target){
+    int left = 0;
+    int right = numsSize - 1;
+    int mid = 0;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return left;
+}
