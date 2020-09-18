15770c63feb6ed001c5303610de57e567a6c439b
xinleiye <xinlei_ye@163.com>
2020-09-18 17:09:56 +0800

    每日一题 全排列 II 添加注释

--- 每日一题/2020年9月份/第18题 47. 全排列 II/javascript/ac_v1.js
+++ 每日一题/2020年9月份/第18题 47. 全排列 II/javascript/ac_v1.js
@@ -13,6 +13,7 @@ var permuteUnique = function(nums) {
             return;
         }
         for (let i = 0; i < nums.length; i++) {
+            // 最后 !visited[i - 1] 作用：确保相同的数字按照从左到右的顺序添加，以此达到去重的目的。
             if (visited[i] || (i > 0 && nums[i] === nums[i - 1] && !visited[i - 1])) {
                 continue;
             }
