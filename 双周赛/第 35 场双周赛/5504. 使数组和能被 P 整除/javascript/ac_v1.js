80ce9c7f27e8de5379969dcea8d8c9d1ba55a5ae
yexinlei <xinlei_ye@hotmail.com>
2020-09-22 15:24:47 +0800

    弟 35 场双周赛 使数组和能被 P 整除 ac

--- 双周赛/第 35 场双周赛/5504. 使数组和能被 P 整除/javascript/ac_v1.js
+++ 双周赛/第 35 场双周赛/5504. 使数组和能被 P 整除/javascript/ac_v1.js
@@ -4,5 +4,33 @@
  * @return {number}
  */
 var minSubarray = function(nums, p) {
+    let res = nums.length + 1;
+    let totalSum = 0;
+    let sumMod = new Array(nums.length + 1);
+    let sumModMap = new Map();
 
+    sumMod[0] = 0;
+    for (let i = 0; i < nums.length; i++) {
+        totalSum += nums[i];
+        sumMod[i + 1] = (nums[i] + sumMod[i]) % p;
+    }
+
+    if (totalSum < p) {
+        return -1;
+    }
+    if (sumMod[nums.length] === 0) {
+        return 0;
+    }
+
+    sumModMap.set(0, 0);
+    for (let i = 1; i <= nums.length; i++) {
+        let preMod = (sumMod[i] - sumMod[nums.length] + p) % p;
+
+        if (sumModMap.has(preMod)) {
+            res = Math.min(res, i - sumModMap.get(preMod));
+        }
+        sumModMap.set(sumMod[i], i);
+    }
+
+    return res >= nums.length ? -1 : res;
 };
