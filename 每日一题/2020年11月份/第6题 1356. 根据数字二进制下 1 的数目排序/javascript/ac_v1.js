a779cb769cba8fa7cc005f6edcb293354a0568c4
yexinlei <xinlei_ye@hotmail.com>
2020-11-06 18:02:17 +0800

    每日一题 根据数字二进制下 1 的数目排序

---
+++ 每日一题/2020年11月份/第6题 1356. 根据数字二进制下 1 的数目排序/javascript/ac_v1.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function(arr) {
+    let arrMap = new Map();
+    let get1Count = (num) => {
+        let count = 0;
+
+        while (num > 0) {
+            if (num & 1) {
+                count++;
+            }
+            num = num >>> 1;
+        }
+        return count;
+    };
+
+
+    arr.sort((val1, val2) => {
+        let diff = get1Count(val1) - get1Count(val2);
+
+        if (diff === 0) {
+            return val1 -  val2;
+        } else {
+            return diff;
+        }
+    });
+
+    return arr;
+};
