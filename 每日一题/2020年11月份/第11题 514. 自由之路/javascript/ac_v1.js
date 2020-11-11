ab119dfe753fb78c04310bc7fbe78c197336f7bc
xinlei_ye@163.com <xinlei_ye@163.com>
2020-11-11 18:02:31 +0800

    每日一题 自由之路

---
+++ 每日一题/2020年11月份/第11题 514. 自由之路/javascript/ac_v1.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} ring
+ * @param {string} key
+ * @return {number}
+ */
+var findRotateSteps = function(ring, key) {
+    let steps = new Array(key.length);
+    let charMap = new Map();
+
+    for (let i = 0; i < key.length; i++) {
+        steps[i] = new Array(ring.length).fill(Number.MAX_SAFE_INTEGER);
+    }
+
+    for (let i = 0; i < ring.length; i++) {
+        if (charMap.has(ring[i])) {
+            charMap.get(ring[i]).push(i);
+        } else {
+            charMap.set(ring[i], [i]);
+        }
+    }
+    console.log(charMap);
+    for (let item of charMap.get(key[0])) {
+        console.log(item);
+        steps[0][i] = Math.min(item, ring.length - item) + 1;
+    }
+
+    for (let i = 1; i < key.length; i++) {
+        for (let index of charMap.get(key[i])) {
+            for (let preIndex of charMap.get(key[i - 1])) {
+                steps[i][index] = Math.min(steps[i][index], steps[i - 1][preIndex] + )
+            }
+        }
+    }
+};
