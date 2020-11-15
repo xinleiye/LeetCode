61dad8c960bb305eb686bd3c9b7644cfde9f2d54
xinlei_ye <xinlei_ye@163.com>
2020-11-15 23:51:37 +0800

    第 215 场周赛

---
+++ 周赛/第 215 场周赛/5602. 确定两个字符串是否接近/javascript/ac_v1.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var closeStrings = function(word1, word2) {
+    let word1Char = new Array(26).fill(0);
+    let word2Char = new Array(26).fill(0);
+
+    if (word1.length !== word2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < word1.length; i++) {
+        word1Char[word1.charCodeAt(i) - 97]++;
+    }
+
+    for (let i = 0; i < word2.length; i++) {
+        word2Char[word2.charCodeAt(i) - 97]++;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if ((word1Char[i] > 0 && word2Char[i] === 0) ||
+            (word1Char[i] === 0 && word2Char[i] > 0)) {
+            return false;
+        }
+    }
+
+    word1Char.sort((val1, val2) => {
+        return val1 - val2;
+    });
+    word2Char.sort((val1, val2) => {
+        return val1 - val2;
+    });
+    for (let i = 0; i < 26; i++) {
+        if (word1Char[i] !== word2Char[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
