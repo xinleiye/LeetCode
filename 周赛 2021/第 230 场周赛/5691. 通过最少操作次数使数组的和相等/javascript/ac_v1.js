4ed538deec8a85381954279f97f38a3b79b7a7d7
yexinlei <xinlei_ye@hotmail.com>
2021-03-05 11:38:46 +0800

    第 230 场周赛 通过最少操作次数使数组的和相等

--- 周赛 2021/第 230 场周赛/5691. 通过最少操作次数使数组的和相等/javascript/ac_v1.js
+++ 周赛 2021/第 230 场周赛/5691. 通过最少操作次数使数组的和相等/javascript/ac_v1.js
@@ -4,5 +4,46 @@
  * @return {number}
  */
 var minOperations = function(nums1, nums2) {
+    let res = 0;
+    let diff;
+    const sum1 = calcArrSum(nums1);
+    const sum2 = calcArrSum(nums2);
+    const freq = new Array(6).fill(0);
 
+    if (sum1 === sum2) {
+        return 0;
+    }
+    if (sum1 < sum2) {
+        const tmpNums = nums1;
+
+        nums1 = nums2;
+        nums2 = tmpNums;
+    }
+
+    nums1.forEach(val => {
+        freq[val - 1]++;
+    });
+    nums2.forEach(val => {
+        freq[6- val]++;
+    });
+
+    diff = Math.abs(sum1 - sum2);
+    for (let i = 5; i > 0; i--) {
+        while (freq[i] > 0 && diff > 0) {
+            freq[i]--;
+            diff -= i;
+            res++;
+        }
+        if (diff < 0) {
+            break;
+        }
+    }
+
+    return diff > 0 ? -1 : res;
 };
+
+function calcArrSum (arr) {
+    return arr.reduce((preVal, currentVal) => {
+        return preVal + currentVal;
+    });
+}
