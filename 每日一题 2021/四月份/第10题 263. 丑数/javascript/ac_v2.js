82b68465b08003b9bec0b44e88e4340be58be587
xinlei_ye <xinlei_ye@163.com>
2021-04-11 21:48:19 +0800

    每日一题 丑数 II

---
+++ 每日一题 2021/四月份/第10题 263. 丑数/javascript/ac_v2.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isUgly = function(n) {
+    let ugly = (num) => {
+        if (num === 1) {
+            return true;
+        }
+        return ((num % 2 === 0) && ugly(num / 2)) ||
+            ((num % 3 === 0) && ugly(num / 3)) ||
+            ((num % 5 === 0) && ugly(num / 5));
+    };
+
+    if (n <= 0) {
+        return false;
+    }
+
+    return ugly(n);
+};
