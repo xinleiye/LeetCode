7626490af5e531b73bbcf503333af169eb371d2d
xinlei_ye <xinlei_ye@163.com>
2021-05-15 21:51:29 +0800

    每日一题 罗马数字转整数

---
+++ 每日一题 2021/五月份/第15题 13. 罗马数字转整数/javascript/ac_v1.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {
+    let res = 0;
+    let pre = s[0];
+    const romanMap = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    res += romanMap[pre];
+    for (let i = 1; i < s.length; i++) {
+        if ((pre === "I" && (s[i] === "V" || s[i] === "X")) ||
+            (pre === "X" && (s[i] === "L" || s[i] === "C")) ||
+            (pre === "C" && (s[i] === "D" || s[i] === "M"))) {
+            res = res - romanMap[pre] + (romanMap[s[i]] - romanMap[pre]);
+        } else {
+            res += romanMap[s[i]];
+            pre = s[i];
+        }
+    }
+
+    return res;
+};
