c3cb7ffac76cc58d154b2b3a4f25ef3ea2df478a
yexinlei <xinlei_ye@hotmail.com>
2021-05-21 00:04:43 +0800

    每日一题 前K个高频单词

--- 每日一题 2021/五月份/第20题 692. 前K个高频单词/javascript/ac_v1.js
+++ 每日一题 2021/五月份/第20题 692. 前K个高频单词/javascript/ac_v1.js
@@ -1,33 +1,38 @@
 /**
- * @param {number[][]} matrix
+ * @param {string[]} words
  * @param {number} k
- * @return {number}
+ * @return {string[]}
  */
-var kthLargestValue = function(matrix, k) {
-    const maxRow = matrix.length;
-    const maxCol = matrix[0].length;
-    const buff = new Array(maxRow * maxCol);
-    let buffIndex = 1;
+var topKFrequent = function(words, k) {
+    let wordMap = new Map();
+    let wordArr = [];
+    let wordCount = [];
+    let wordIndexMap = new Map();
 
-    buff[0] = matrix[0][0];
-    for (let i = 1; i < maxCol; i++) {
-        matrix[0][i] ^= matrix[0][i - 1];
-        buff[buffIndex++] = matrix[0][i];
-    }
-    for (let i = 1; i < maxRow; i++) {
-        matrix[i][0] ^= matrix[i - 1][0];
-        buff[buffIndex++] = matrix[i][0];
-    }
-    for (let i = 1; i < maxRow; i++) {
-        for (let j = 1; j < maxCol; j++) {
-            matrix[i][j] ^= matrix[i - 1][j - 1] ^ matrix[i][j - 1] ^ matrix[i - 1][j];
-            buff[buffIndex++] = matrix[i][j];
+    words.forEach(word => {
+        if (wordMap.has(word)) {
+            wordMap.set(word, wordMap.get(word) + 1);
+        } else {
+            wordMap.set(word, 1);
+            wordArr.push(word)
         }
-    }
-
-    buff.sort((val1, val2) => {
-        return val2 - val1;
     });
 
-    return buff[k - 1];
+    wordArr.forEach((word, index) => {
+        wordCount.push(wordMap.get(word));
+        wordIndexMap.set(word, index);
+    });
+
+    wordArr.sort((word1, word2) => {
+        const count1 = wordCount[wordIndexMap.get(word1)];
+        const count2 = wordCount[wordIndexMap.get(word2)];
+
+        if (count1 === count2) {
+            return word1 > word2 ? 1 : -1;
+        } else {
+            return count2 - count1;
+        }
+    });
+
+    return wordArr.slice(0, k);
 };
