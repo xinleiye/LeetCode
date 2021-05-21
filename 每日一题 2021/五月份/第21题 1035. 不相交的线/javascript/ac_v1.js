847b6306adca126576ebff588aa13dce79e73aa0
xinlei_ye@163.com <xinlei_ye@163.com>
2021-05-21 17:12:43 +0800

    每日一题 不相交的线

---
+++ 每日一题 2021/五月份/第21题 1035. 不相交的线/javascript/ac_v1.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxUncrossedLines = function(nums1, nums2) {
+    const res = new Array(nums1.length + 1);
+    const length1 = nums1.length;
+    const length2 = nums2.length;
+
+    for (let i = 0; i <= length1; i++) {
+        res[i] = new Array(length2 + 1).fill(0);
+    }
+    for (let i = 1; i <= length1; i++) {
+        for (let j = 1; j <= length2; j++) {
+            if (nums1[i - 1] === nums2[j - 1]) {
+                res[i][j] = res[i - 1][j - 1] + 1;
+            } else {
+                res[i][j] = Math.max(res[i - 1][j], res[i][j - 1]);
+            }
+        }
+    }
+
+    return res[length1][length2];
+};
