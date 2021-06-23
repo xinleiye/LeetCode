bae377dd995bcf3f3d74868a60a1ff2acfa573c6
yexinlei <xinlei_ye@hotmail.com>
2021-06-23 23:27:11 +0800

    每日一题 二进制中1的个数

---
+++ 每日一题 2021/六月份/第23题 剑指 Offer 15. 二进制中1的个数/javascript/ac_v1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let res = 0;
+
+    while (n) {
+        if (n & 1 === 1) {
+            res++;
+        }
+        n = n >>> 1;
+    }
+
+    return res;
+};
