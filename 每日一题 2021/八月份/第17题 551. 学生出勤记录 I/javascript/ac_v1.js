a03fdd5908dfb4062f711d7f04b2bad8fecb23d7
yexinlei <xinlei_ye@hotmail.com>
2021-08-17 23:57:55 +0800

    每日一题 551. 学生出勤记录 I ac

--- 每日一题 2021/八月份/第17题 551. 学生出勤记录 I/javascript/ac_v1.js
+++ 每日一题 2021/八月份/第17题 551. 学生出勤记录 I/javascript/ac_v1.js
@@ -3,5 +3,25 @@
  * @return {boolean}
  */
 var checkRecord = function(s) {
+    let absent = 0;
+    let late = 0;
 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "A") {
+            absent++;
+            late = 0;
+            if (absent >= 2) {
+                return false;
+            }
+        } else if (s[i] === "L") {
+            late++;
+            if (late >= 3) {
+                return false;
+            }
+        } else {
+            late = 0;
+        }
+    }
+
+    return true;
 };
