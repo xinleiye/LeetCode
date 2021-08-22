6ab9267b503e69eafa2580240979e24b05f23214
yexinlei <xinlei_ye@hotmail.com>
2021-08-22 23:43:54 +0800

    第 255 场周赛

--- 周赛 2021/第 254 场周赛/5846. 你能穿过矩阵的最后一天/javascript/ac_v1.js
+++ 周赛 2021/第 254 场周赛/5846. 你能穿过矩阵的最后一天/javascript/ac_v1.js
@@ -1,38 +1,9 @@
 /**
- * @param {number[]} obstacles
- * @return {number[]}
+ * @param {number} row
+ * @param {number} col
+ * @param {number[][]} cells
+ * @return {number}
  */
-var longestObstacleCourseAtEachPosition = function(obstacles) {
-    let res = new Array(obstacles.length);
-    let sorted = new Array(obstacles.length + 1).fill(0);
-    let length = 1;
+var latestDayToCross = function(row, col, cells) {
 
-    res[0] = length;
-    sorted[length] = obstacles[0];
-    for (let i = 1, len = obstacles.length; i < len; i++) {
-        if (obstacles[i] >= sorted[length]) {
-            length++;
-            res[i] = length;
-            sorted[length] = obstacles[i];
-        } else {
-            let left = 1;
-            let right = length;
-            let index = 0;
-
-            while (left <= right) {
-                const mid = (left + right) >>> 1;
-
-                if (sorted[mid] <= obstacles[i]) {
-                    left = mid + 1;
-                } else {
-                    index = mid;
-                    right = mid - 1;
-                }
-            }
-            res[i] = index;
-            sorted[index] = obstacles[i];
-        }
-    }
-
-    return res;
 };
