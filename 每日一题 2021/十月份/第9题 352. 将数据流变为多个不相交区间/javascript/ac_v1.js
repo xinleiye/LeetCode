e7fb69d49722e0b25a014186d123ae87318b7305
yexinlei <xinlei_ye@hotmail.com>
2021-10-09 23:23:10 +0800

    每日一题 352. 将数据流变为多个不相交区间

---
+++ 每日一题 2021/十月份/第9题 352. 将数据流变为多个不相交区间/javascript/ac_v1.js
@@ -0,0 +1,25 @@
+var SummaryRanges = function() {
+
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+SummaryRanges.prototype.addNum = function(val) {
+
+};
+
+/**
+ * @return {number[][]}
+ */
+SummaryRanges.prototype.getIntervals = function() {
+
+};
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * var obj = new SummaryRanges()
+ * obj.addNum(val)
+ * var param_2 = obj.getIntervals()
+ */
