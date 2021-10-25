97995e5e00903d4285d5743ba23bc3d230cb7e18
yexinlei <xinlei_ye@hotmail.com>
2021-10-25 17:15:04 +0800

    每日一题 240. 搜索二维矩阵 II

---
+++ 每日一题 2021/十月份/第25题 240. 搜索二维矩阵 II/javascript/ac_v1.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length - 1;
+    const maxRow = matrix.length;
+
+    while (row < maxRow && col >= 0) {
+        if (matrix[row][col] < target) {
+            row++;
+        } else if (matrix[row][col] > target) {
+            col--;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+};
