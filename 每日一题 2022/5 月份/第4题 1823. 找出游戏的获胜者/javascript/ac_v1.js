1fa89932ee4e939ed6ba56ac1e8e0c2a5293d4e5
xinlei_ye <xinlei_ye@163.com>
2022-05-04 23:56:08 +0800

    每日一题 1823. 找出游戏的获胜者

---
+++ 每日一题 2022/5 月份/第4题 1823. 找出游戏的获胜者/javascript/ac_v1.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findTheWinner = function(n, k) {
+    let index = 0;
+    let peoples = new Array(n);
+
+    for (let i = 0; i < n; i++) {
+        peoples[i] = i + 1;
+    }
+
+    while (peoples.length !== 1) {
+        index = (index + k - 1) % peoples.length;
+        peoples.splice(index, 1);
+    }
+
+    return peoples[0];
+};
