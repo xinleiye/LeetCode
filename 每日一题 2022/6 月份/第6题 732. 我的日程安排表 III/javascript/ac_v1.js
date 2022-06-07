e13beb7a5386b74bcaf800905acd946f2a1ee4c8
yexinlei <xinlei_ye@hotmail.com>
2022-06-07 23:39:16 +0800

    732. 我的日程安排表 III ac

--- 每日一题 2022/6 月份/第6题 732. 我的日程安排表 III/javascript/ac_v1.js
+++ 每日一题 2022/6 月份/第6题 732. 我的日程安排表 III/javascript/ac_v1.js
@@ -1,6 +1,5 @@
-
 var MyCalendarThree = function() {
-
+    this.map = new Map();
 };
 
 /** 
@@ -9,7 +8,22 @@ var MyCalendarThree = function() {
  * @return {number}
  */
 MyCalendarThree.prototype.book = function(start, end) {
+    let res = 0;
+    let max = 0;
+    const calender = [];
 
+    this.map.set(start, (this.map.get(start) || 0) + 1);
+    this.map.set(end, (this.map.get(end) || 0) - 1);
+    this.map.forEach((val, key) => {
+        calender.push([key, val]);
+    });
+    calender.sort((c1, c2) => c1[0] - c2[0]);
+    for (const c of calender) {
+        max += c[1];
+        res = Math.max(res, max);
+    }
+
+    return res;
 };
 
 /**
