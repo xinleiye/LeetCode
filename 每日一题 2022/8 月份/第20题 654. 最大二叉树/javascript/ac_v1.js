bbbbcca7f2646fe9f4c346823aed4c567280506d
xinlei_ye <xinlei_ye@163.com>
2022-08-21 21:08:08 +0800

    2022.08.20 每日一题 654. 最大二叉树

--- 每日一题 2022/8 月份/第20题 654. 最大二叉树/javascript/ac_v1.js
+++ 每日一题 2022/8 月份/第20题 654. 最大二叉树/javascript/ac_v1.js
@@ -11,5 +11,26 @@
  * @return {TreeNode}
  */
 var constructMaximumBinaryTree = function(nums) {
+    const generateTree = (left, right) => {
+        if (right < left) {
+            return null;
+        }
 
+        let index = left;
+        let max = nums[left];
+        for (let i = left; i <= right; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+
+        const node = new TreeNode(max);
+        node.left = generateTree(left, index - 1);
+        node.right = generateTree(index + 1, right);
+
+        return node;
+    };
+
+    return generateTree(0, nums.length - 1);
 };
