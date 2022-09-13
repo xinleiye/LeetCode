41e9a5761eb1c277c438e174961f8522c5071a20
yexinlei <xinlei_ye@hotmail.com>
2022-09-13 21:42:48 +0800

    2022.09.08 每日一题 667. 优美的排列 II

--- 每日一题 2022/9 月份/第8题 667. 优美的排列 II/javascript/ac_v1.js
+++ 每日一题 2022/9 月份/第8题 667. 优美的排列 II/javascript/ac_v1.js
@@ -4,5 +4,21 @@
  * @return {number[]}
  */
 var constructArray = function(n, k) {
+    let index = 0;
+    const res = new Array(n);
 
+    for (let i = 1; i < n - k; i++) {
+        res[index] = i;
+        index++;
+    }
+    for (let i = n - k, j = n; i <= j; i++, j--) {
+        res[index] = i;
+        index++;
+        if (i !== j) {
+            res[index] = j;
+            index++;
+        }
+    }
+
+    return res;
 };
