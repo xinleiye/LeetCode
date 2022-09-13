56fd043f3289b9fdca625139da3b52565c0b5332
yexinlei <xinlei_ye@hotmail.com>
2022-09-13 21:35:58 +0800

    每日一题 670. 最大交换

--- 每日一题 2022/9 月份/第12题 1608. 特殊数组的特征值/javascript/ac_v1.js
+++ 每日一题 2022/9 月份/第12题 1608. 特殊数组的特征值/javascript/ac_v1.js
@@ -1,9 +1,16 @@
 /**
- * @param {number[]} quality
- * @param {number[]} wage
- * @param {number} k
+ * @param {number[]} nums
  * @return {number}
  */
-var mincostToHireWorkers = function(quality, wage, k) {
+var specialArray = function(nums) {
+    const total = nums.length;
 
+    nums.sort((n1, n2) => n2 - n1);
+    for (let i = 1;  i <= total; i++) {
+        if (nums[i - 1] >= i && (i === total || nums[i] < i)) {
+            return i;
+        }
+    }
+
+    return -1;
 };
