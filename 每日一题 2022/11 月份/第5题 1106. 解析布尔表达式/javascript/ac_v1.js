2343a5a8d92a12706aba534480c8457ec1dcdcdd
yexinlei <xinlei_ye@hotmail.com>
2022-11-07 22:30:44 +0800

    每日一题 816. 模糊坐标

--- 每日一题 2022/11 月份/第5题 1106. 解析布尔表达式/javascript/ac_v1.js
+++ 每日一题 2022/11 月份/第5题 1106. 解析布尔表达式/javascript/ac_v1.js
@@ -1,14 +1,7 @@
 /**
- * @param {string} sequence
- * @param {string} word
- * @return {number}
+ * @param {string} expression
+ * @return {boolean}
  */
-var maxRepeating = function(sequence, word) {
-    for (let i = Math.floor(sequence.length / word.length); i >= 0; i--) {
-        if (sequence.indexOf(word.repeat(i)) >= 0) {
-            return i;
-        }
-    }
+var parseBoolExpr = function(expression) {
 
-    return 0;
 };
