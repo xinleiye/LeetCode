c311add284c3feae49808e70cdba70bcc5c7caae
yexinlei <xinlei_ye@hotmail.com>
2022-12-08 21:44:33 +0800

    每日一题 1812. 判断国际象棋棋盘中一个格子的颜色

---
+++ 每日一题 2022/12 月份/第8题 1812. 判断国际象棋棋盘中一个格子的颜色/javascript/ac_v1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+ var squareIsWhite = function(coordinates) {
+    const colorObj = {
+        a: 1,
+        b: 0,
+        c: 1,
+        d: 0,
+        e: 1,
+        f: 0,
+        g: 1,
+        h: 0
+    };
+    const [x, y] = coordinates.split("");
+
+    return colorObj[x] === (Number(y) + 1) % 2;
+};
