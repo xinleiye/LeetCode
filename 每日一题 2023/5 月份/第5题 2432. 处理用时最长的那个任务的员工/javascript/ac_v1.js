aadaf00ff80b8db57aff05886b7feb81b9e6aec0
yexinlei <xinlei_ye@hotmail.com>
2023-05-05 22:11:10 +0800

    每日一题 2432. 处理用时最长的那个任务的员工

---
+++ 每日一题 2023/5 月份/第5题 2432. 处理用时最长的那个任务的员工/javascript/ac_v1.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number[][]} logs
+ * @return {number}
+ */
+var hardestWorker = function(n, logs) {
+    let res = 0;
+    let maxTime = 0;
+    let curTime = 0;
+
+    for (const [id, leaveTime] of logs) {
+        const time = leaveTime - curTime;
+
+        if (time > maxTime) {
+            maxTime = time;
+            res = id;
+        } else if (time === maxTime && id < res) {
+            res = id;
+        }
+        curTime = leaveTime;
+    }
+
+    return res;
+};
