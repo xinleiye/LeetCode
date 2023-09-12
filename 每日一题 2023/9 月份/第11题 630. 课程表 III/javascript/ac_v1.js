cd353068fbde24394b391722351ed2931dcf3641
yexinlei <xinlei_ye@hotmail.com>
2023-09-12 23:50:21 +0800

    每日一题 1462. 课程表 IV

--- 每日一题 2023/9 月份/第11题 630. 课程表 III/javascript/ac_v1.js
+++ 每日一题 2023/9 月份/第11题 630. 课程表 III/javascript/ac_v1.js
@@ -3,5 +3,24 @@
  * @return {number}
  */
 var scheduleCourse = function(courses) {
+    let time = 0;
+    const maxQueue = new MaxPriorityQueue();
 
+    courses.sort((c1, c2) => c1[1] - c2[1]);
+    for (const [duration, lastDay] of courses) {
+        if (time + duration <= lastDay) {
+            time += duration;
+            maxQueue.enqueue(duration)
+        } else if (!maxQueue.isEmpty()) {
+            const max = maxQueue.front().element;
+
+            if (max > duration) {
+                time -= (max - duration);
+                maxQueue.dequeue();
+                maxQueue.enqueue(duration);
+            }
+        }
+    }
+
+    return maxQueue.size();
 };
