2fe3841d44577d1daa574f54967d5b1e44f94429
yexinlei <xinlei_ye@hotmail.com>
2023-09-17 23:14:49 +0800

    每日一题 213. 打家劫舍 II

---
+++ 每日一题 2023/9 月份/第17题 213. 打家劫舍 II/javascript/ac_v1.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    const length = nums.length;
+    const robRange = (start, end) => {
+        let pre2 = nums[start];
+        let pre1 = Math.max(nums[start], nums[start + 1]);
+
+        for (let i = start + 2; i <= end; i++) {
+            let tmp = pre1;
+
+            pre1 = Math.max(nums[i] + pre2, pre1);
+            pre2 = tmp;
+        }
+
+        return pre1;
+    };
+
+    if (length === 1) {
+        return nums[0];
+    } else if (length === 2) {
+        return Math.max(nums[0], nums[1]);
+    }
+
+    return Math.max(robRange(0, length - 2), robRange(1, length - 1));
+};
