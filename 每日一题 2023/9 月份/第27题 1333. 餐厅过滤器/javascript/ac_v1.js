52a218550735243cc3f8039a2bbc1923e0a3e107
yexinlei <xinlei_ye@hotmail.com>
2023-09-28 00:35:03 +0800

    每日一题 1333. 餐厅过滤器 ac

--- 每日一题 2023/9 月份/第27题 1333. 餐厅过滤器/javascript/ac_v1.js
+++ 每日一题 2023/9 月份/第27题 1333. 餐厅过滤器/javascript/ac_v1.js
@@ -6,5 +6,25 @@
  * @return {number[]}
  */
 var filterRestaurants = function(restaurants, veganFriendly, maxPrice, maxDistance) {
+    const res = [];
+    const target = restaurants.filter(item => {
+        if (veganFriendly === 0) {
+            return item[3] <= maxPrice && item[4] <= maxDistance;
+        } else {
+            return item[2] === veganFriendly && item[3] <= maxPrice && item[4] <= maxDistance;
+        }
+    });
 
+    target.sort((t1, t2) => {
+        if (t1[1] !== t2[1]) {
+            return t2[1] - t1[1];
+        } else {
+            return t2[0] - t1[0];
+        }
+    });
+    for (const item of target) {
+        res.push(item[0]);
+    }
+
+    return res;
 };
