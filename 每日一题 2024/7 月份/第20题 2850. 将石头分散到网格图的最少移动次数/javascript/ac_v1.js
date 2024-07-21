76fc73fe0f3da67ef7b633f0c35a3ed7716f67e4
xinlei_ye <xinlei_ye@hotmail.com>
2024-07-21 23:56:28 +0800

    每日一题 1186. 删除一次得到子数组最大和

--- 每日一题 2024/7 月份/第20题 2850. 将石头分散到网格图的最少移动次数/javascript/ac_v1.js
+++ 每日一题 2024/7 月份/第20题 2850. 将石头分散到网格图的最少移动次数/javascript/ac_v1.js
@@ -1,27 +1,7 @@
 /**
- * @param {number[]} possible
+ * @param {number[][]} grid
  * @return {number}
  */
-var minimumLevels = function(possible) {
-    let a = 0;
-    let b = 0;
-    const total = possible.length;
+var minimumMoves = function(grid) {
 
-    for (let i = 0; i < total; i++) {
-        b += possible[i] === 1 ? 1 : -1;
-    }
-    for (let i = 0; i < total - 1; i++) {
-        if (possible[i] === 1) {
-            a += 1;
-            b -= 1;
-        } else {
-            a -= 1;
-            b += 1;
-        }
-        if (a > b) {
-            return i + 1;
-        }
-    }
-
-    return -1;
 };
