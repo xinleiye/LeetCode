cef56fef8f5bf1e36939e0e7db1a9ae9142b2171
xinlei_ye <xinlei_ye@hotmail.com>
2024-12-03 17:12:29 +0800

    每日一题 2576. 求出最多标记下标

--- 每日一题 2024/9 月份/第12题 2576. 求出最多标记下标/javascript/ac_v1.js
+++ 每日一题 2024/9 月份/第12题 2576. 求出最多标记下标/javascript/ac_v1.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var maxNumOfMarkedIndices = function(nums) {
+    nums.sort((n1, n2) => n1 - n2);
 
+    const total = nums.length;
+    const half = total >>> 1;
+    let left = 0;
+    let right = total >>> 1;
+    let res = 0;
+    while (left < half && right < total) {
+        while (right < total && nums[left] * 2 > nums[right]) {
+            right++;
+        }
+        if (right < total) {
+            res += 2;
+            right++;
+        }
+        left++;
+    }
+
+    return res;
 };
