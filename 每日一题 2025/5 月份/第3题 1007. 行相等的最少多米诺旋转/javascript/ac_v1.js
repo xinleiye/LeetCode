4daa268dace5a56d0348beccb9eec9696dd71516
xinlei_ye <xinlei_ye@hotmail.com>
2025-05-05 00:07:41 +0800

    每日一题 1128. 等价多米诺骨牌对的数量

--- 每日一题 2025/5 月份/第3题 1007. 行相等的最少多米诺旋转/javascript/ac_v1.js
+++ 每日一题 2025/5 月份/第3题 1007. 行相等的最少多米诺旋转/javascript/ac_v1.js
@@ -1,8 +1,20 @@
 /**
- * @param {number[]} tops
- * @param {number[]} bottoms
+ * @param {number[][]} dominoes
  * @return {number}
  */
-var minDominoRotations = function(tops, bottoms) {
-    
+var numEquivDominoPairs = function(dominoes) {
+    let res = 0;
+    const getHash = (num1, num2) => 0 | (1 << (num1 - 1)) | (1 << (num2 -1));
+    const map = new Map();
+
+    for (const [d1, d2] of dominoes) {
+        const hash = getHash(d1, d2);
+
+        map.set(hash, (map.get(hash) || 0) + 1);
+    }
+    map.forEach(count => {
+        res += count * (count - 1) / 2;
+    });
+
+    return res;
 };
