d8084c04ba2258e9ea93601da3de73ba92df49c5
xinlei_ye <xinlei_ye@hotmail.com>
2025-05-07 23:45:52 +0800

    每日一题 3341. 到达最后一个房间的最少时间 I

---
+++ 每日一题 2025/5 月份/第7题 3341. 到达最后一个房间的最少时间 I/javascript/ac_v1.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} moveTime
+ * @return {number}
+ */
+var minTimeToReach = function(moveTime) {
+    const n = moveTime.length;
+    const m = moveTime[0].length;
+    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]; 
+    const d = Array.from({ length: n }, () => new Array(m).fill(Infinity));
+    const v = Array.from({ length: n }, () => new Array(m).fill(false));
+    const q = new PriorityQueue((a, b) => {
+        return a.dist < b.dist ? -1 : 1;
+    });
+    d[0][0] = 0;
+    q.enqueue({x: 0, y: 0, dist: 0});
+    while (!q.isEmpty()) {
+        const s = q.dequeue();
+        if (v[s.x][s.y]) {
+            continue;
+        }
+        v[s.x][s.y] = true;
+        for (const [dx, dy] of dirs) {
+            const nx = s.x + dx;
+            const ny = s.y + dy;
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
+
+            const dist = Math.max(d[s.x][s.y], moveTime[nx][ny]) + 1;
+            if (d[nx][ny] > dist) {
+                d[nx][ny] = dist;
+                q.enqueue({x: nx, y: ny, dist: dist});
+            }
+        }
+    }
+
+    return d[n - 1][m - 1];
+};
