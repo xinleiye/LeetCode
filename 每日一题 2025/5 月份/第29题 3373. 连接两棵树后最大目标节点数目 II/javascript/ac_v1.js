1ec2a0bc5bc9c845bfe66c51759bf19284e25334
xinlei_ye <xinlei_ye@hotmail.com>
2025-05-29 23:59:03 +0800

    每日一题 3373. 连接两棵树后最大目标节点数目 II

--- 每日一题 2025/5 月份/第29题 3373. 连接两棵树后最大目标节点数目 II/javascript/ac_v1.js
+++ 每日一题 2025/5 月份/第29题 3373. 连接两棵树后最大目标节点数目 II/javascript/ac_v1.js
@@ -1,31 +1,34 @@
 /**
  * @param {number[][]} edges1
  * @param {number[][]} edges2
- * @param {number} k
  * @return {number[]}
  */
-var maxTargetNodes = function(edges1, edges2, k) {
+
+const COLOR1 = 0;
+const COLOR2 = 1;
+var maxTargetNodes = function(edges1, edges2) {
     const total1 = edges1.length + 1;
     const total2 = edges2.length + 1;
     const res = new Array(total1).fill(0);
-    const count1 = new Array(total1).fill(0);
-    const count2 = new Array(total2).fill(0);
+    const colors1 = new Array(total1).fill(0);
+    const colors2 = new Array(total2).fill(0);
     const map1 = new Map();
     const map2 = new Map();
-    let maxCount2 = 0;
+    let color11 = 0, color12 = 0;
+    let color21 = 0, max2Color = 0;
+
     genMap(map1, edges1);
     genMap(map2, edges2);
+    color11 = dfsSearch(map1, 0 , -1, 0, colors1);
+    color12 = total1 - color11;
+    color21 = dfsSearch(map2, 0 , -1, 0, colors2);
+    max2Color = Math.max(color21, total2 - color21);
     for (let i = 0; i < total1; i++) {
-        count1[i] = dfsSearch(map1, i , -1, k);
-    }
-    for (let i = 0; i < total2; i++) {
-        count2[i] = dfsSearch(map2, i , -1, k - 1);
-    }
-    for (const c of count2) {
-        maxCount2 = Math.max(maxCount2, c);
-    }
-    for (let i = 0; i < total1; i++) {
-        res[i] = count1[i] + maxCount2;
+        if (colors1[i] === COLOR1) {
+            res[i] = color11 + max2Color;
+        } else {
+            res[i] = color12 + max2Color;
+        }
     }
 
     return res;
@@ -44,18 +47,16 @@ function genMap(map, edge) {
     }
 }
 
-function dfsSearch(map, cur, parent, target) {
-    if (target < 0) {
-        return 0;
-    }
-
-    let count = 1;
+function dfsSearch(map, cur, parent, depth, color) {
+    let count = 1 - depth % 2;
     const next = map.get(cur) || [];
+
+    color[cur] = depth % 2 === 0 ? COLOR1 : COLOR2;
     for (const n of next) {
         if (n === parent) {
             continue;
         }
-        count += dfsSearch(map, n, cur, target - 1);
+        count += dfsSearch(map, n, cur, depth + 1, color);
     }
 
     return count;
